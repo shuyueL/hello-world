60600a25b6e65073f07313390b7a9a554486b460
shuyueL <lanshyue@gmail.com>
2017-07-31 19:07:03 -0700

    Update strStr.cpp

--- lint/L1/strStr.cpp
+++ lint/L1/strStr.cpp
@@ -1,4 +1,4 @@
-/* For a given source string and a target string, you should output the first index(from 0) of target string in source string.
+/* 13: For a given source string and a target string, you should output the first index(from 0) of target string in source string.
 
 If target does not exist in source, just return -1.*/
 
