0b0a710c38053f58ca020434926c364299559381
shuyueL <lanshyue@gmail.com>
2017-07-31 19:14:00 -0700

    Update ClosestNumInSortedArray.cpp

--- lint/L2/ClosestNumInSortedArray.cpp
+++ lint/L2/ClosestNumInSortedArray.cpp
@@ -40,3 +40,34 @@ public:
         }
     }
 };
+
+
+// jiuzhang soln
+class Solution {
+public:
+    /**
+     * @param A an integer array sorted in ascending order
+     * @param target an integer
+     * @return an integer
+     */
+    int closestNumber(vector<int>& A, int target) {
+        // Write your code here
+        int l = 0, r = A.size() - 1;
+        if (r < 0)
+            return -1;
+        int ans = abs(A[0] - target);
+        int index = 0;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            if (ans > abs(A[mid] - target)) {
+                ans = abs(A[mid] - target);
+                index = mid;
+            }
+            if (A[mid] >= target)
+                r = mid - 1;
+            else
+                l = mid + 1;
+        }
+        return index;
+    }
+};
