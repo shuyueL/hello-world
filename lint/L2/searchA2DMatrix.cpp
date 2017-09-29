b41ad22132b6c4d7f1d7f8affa30469f1a6d0d58
shuyueL <lanshyue@gmail.com>
2017-09-29 15:30:24 -0700

    Update searchA2DMatrix.cpp

--- lint/L2/searchA2DMatrix.cpp
+++ lint/L2/searchA2DMatrix.cpp
@@ -52,3 +52,49 @@ public:
         return false;
     }
 };
+
+
+// jiuzhang
+class Solution {
+public:
+    /**
+     * @param matrix, a list of lists of integers
+     * @param target, an integer
+     * @return a boolean, indicate whether matrix contains target
+     */
+    bool searchMatrix(vector<vector<int> > &matrix, int target) {
+        int n = matrix.size();
+        if (n == 0) {
+            return false;
+        }
+        
+        int m = matrix[0].size();
+        if (m == 0) {
+            return false;
+        }
+        
+        int start = 0, end = n * m - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            int row = mid / m;
+            int col = mid % m;
+            
+            if (matrix[row][col] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        if (matrix[start / m][start % m] == target) {
+            return true;
+        }
+        
+        if (matrix[end / m][end % m] == target) {
+            return true;
+        }
+        
+        return false;
+    }
+};
+
